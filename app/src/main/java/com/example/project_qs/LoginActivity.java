f8eb6a1f8870212db95cb7cbdf0431a6749dcea6
MariaPacheco2001 <patcheco.f1@gmail.com>
2023-12-21 16:30:31 +0000

    Fiz atualização na classe LoginActivity - João

--- app/src/main/java/com/example/project_qs/LoginActivity.java
+++ app/src/main/java/com/example/project_qs/LoginActivity.java
@@ -7,13 +7,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText editTextUsername, editTextPassword;
     private Button buttonLogin;
+
+    // Utilizando a mesma instância de Autenticacao em toda a aplicação
+    private static Autenticacao autenticacao = new Autenticacao();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +33,14 @@ public class LoginActivity extends AppCompatActivity {
                 String username = editTextUsername.getText().toString();
                 String password = editTextPassword.getText().toString();
 
-                // Verificar as credenciais (substitua isso com sua lógica de autenticação real)
-                if (isValidCredentials(username, password)) {
+                // Verificar as credenciais usando a classe Autenticacao
+                Utilizador utilizadorAutenticado = autenticacao.autenticarUtilizador(username, password);
+
+                if (utilizadorAutenticado != null) {
                     // Credenciais válidas, iniciar a MainActivity
+                    // Também armazenamos o utilizador autenticado na sessão (SessionManager)
+                    SessionManager.setUtilizadorAutenticado(utilizadorAutenticado);
+
                     Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                     startActivity(intent);
                     finish(); // Fecha a LoginActivity para que o usuário não possa voltar a ela pressionando o botão "Voltar"
@@ -43,14 +50,5 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
-    }
-
-    // Função para verificar as credenciais (substitua isso com sua lógica de autenticação real)
-    private boolean isValidCredentials(String username, String password) {
-        // Credenciais válidas: admin1, admin2, admin3, admin4
-        return (username.equals("admin1") && password.equals("admin1")) ||
-                (username.equals("admin2") && password.equals("admin2")) ||
-                (username.equals("admin3") && password.equals("admin3")) ||
-                (username.equals("admin4") && password.equals("admin4"));
     }
 }
